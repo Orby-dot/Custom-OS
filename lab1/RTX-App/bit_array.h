a9e107eccedf4815029bd0978e2243716a01f843
Kevin Kalathil <kkalathil1@gmail.com>
2022-05-21 03:33:18 -0400

    update header functions for bit array

--- lab1/RTX-App/bit_array.h
+++ lab1/RTX-App/bit_array.h
@@ -8,15 +8,16 @@ typedef struct bitArray{
 	U32 startAddress;
 	U32 endAddress;
 	U32 size;
-	U8 bitStatus[((2*(ARRAY_SIZE)/32 -1)/8)];
+	U32 bitStatus[((2*(ARRAY_SIZE)/32 -1)/8)];
 	freeList_t* freeList;
 } bitArray;
 
 void initializeBitArray(bitArray *array,freeList_t * list, U32 startAddress, U32 endAddress);
 U32 allocateNode(bitArray * array, U32 sizeToAllocate);
 void removeNodes(bitArray *array, U32 address);
-U8 getHeight(bitArray*array);
-U8 getXPosition(bitArray* array, U8 address, U8 level);
-int locateNode(bitArray* array, U8 xPosition, U8 level);
-void coalesce(bitArray *array, U8 level, U32 node);
-void updateParentNodes(bitArray *array, U8 level, U32 node);
+U32 getHeight(bitArray*array);
+U32 getXPosition(bitArray* array, U32 address, U32 level);
+int locateNode(bitArray* array, U32 index);
+void coalesce(bitArray *array, U32 level, U32 node);
+void updateParentNodes(bitArray *array, int level, U32 node);
+U32 getBitPositionMask(U32 index);
