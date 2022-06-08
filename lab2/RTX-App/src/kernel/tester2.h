ac9a5482fae29511ed563889b1d314c35dcd3fad
Aryaman Singh <a553sing@uwaterloo.ca>
2022-06-07 20:00:37 -0400

    rename

---
+++ lab2/RTX-App/src/kernel/tester2.h
@@ -0,0 +1,10 @@
+#include "ready_queue.h"
+#include "k_inc.h"
+
+
+void printReadyQueue(readyQueue_t * queue);
+void printLinkList(readyQueue_t list);
+
+void printTCB();
+
+void printKernelStack();
