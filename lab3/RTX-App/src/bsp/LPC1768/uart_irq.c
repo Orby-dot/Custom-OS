dc00ee2f07513727c5a3f8bd30b8d7c56db88fb9
Aryaman Singh <a553sing@uwaterloo.ca>
2022-07-05 02:12:14 -0400

    fake delay

--- lab3/RTX-App/src/bsp/LPC1768/uart_irq.c
+++ lab3/RTX-App/src/bsp/LPC1768/uart_irq.c
@@ -253,7 +253,7 @@ void UART0_IRQHandler(void)
 						int len = 0;
 						while((len+6)<KCD_CMD_BUF_SIZE && msg_buf[len+6] != NULL){
 							char_out = msg_buf[len+6];
-							printf("WC = %c \r\n", char_out);
+                            for (int x = 0; x < 5000; x++);
 							pUart->THR = char_out;
 							len++;
 						}
